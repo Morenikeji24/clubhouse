9a29299ce8df7d8275a331f5499f9cb6e663d3a3
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-16 15:21:21 +0100

    Added a custom express validator for the sign-up form

---
+++ middlewares/formValidator.js
@@ -0,0 +1,41 @@
+import { body, validationResult, matchedData } from "express-validator";
+
+const validateForm = [
+  body("firstname")
+    .trim()
+    .notEmpty()
+    .withMessage("Please fill in your first name")
+    .isAlpha()
+    .withMessage("First name must only be letters"),
+
+  body("lastname")
+    .trim()
+    .notEmpty()
+    .withMessage("Please fill in your last name")
+    .isAlpha()
+    .withMessage("Last name must only be letters"),
+
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Please fill in your username")
+    .isAlphanumeric()
+    .withMessage("Username must only contain letters and numbers"),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+
+  body("password2")
+    .notEmpty()
+    .withMessage("Please confirm your password")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password do not match");
+      }
+      return true;
+    }),
+];
