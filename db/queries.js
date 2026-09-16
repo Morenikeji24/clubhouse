775064c2552cf109d41a8a2bf1fdd92871bc08a4
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-16 16:22:09 +0100

    Created a query for adding users

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,13 @@
 import pool from "./pool.js";
+
+const db = {
+  async createUser(first_name, last_name, username, password) {
+    await pool.query(
+      `INSERT INTO users (first_name, last_name, user_name, password)
+            VALUES ($1, $2, $3, $4)`,
+      [first_name, last_name, username, password],
+    );
+  },
+};
+
+export default db;
