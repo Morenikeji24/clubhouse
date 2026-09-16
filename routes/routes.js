273af36cc319693d77c6f9db6252b8361ef9d3ad
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-16 16:20:26 +0100

    Added routers for the homePage and submitting the sign-up page

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import controller from "../controllers/controller.js";
+import { validateForm } from "../middlewares/formValidator.js";
 
 export const router = Router();
 
+router.get("/", controller.getHomePage);
 router.get("/sign-up", controller.getSignUp);
+router.post("/sign-up", validateForm, controller.postSignUp);
