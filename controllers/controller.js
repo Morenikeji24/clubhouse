1f123ae6bac9b952f3dc61154077e465e69034bf
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-16 16:23:23 +0100

    Added functions to get the homePage and handle the sign-up form submission

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,28 @@
+import { validationResult } from "express-validator";
+import db from "../db/queries.js";
+import bcrypt from "bcryptjs";
+
 const controller = {
+  async getHomePage(req, res) {
+    res.render("index");
+  },
+
   async getSignUp(req, res) {
     res.render("sign-up");
+  },
+
+  async postSignUp(req, res) {
+    const errors = validationResult(req);
+    const { firstname, lastname, username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("sign-up", { errors: errors.array() });
+    }
+
+    await db.createUser(firstname, lastname, username, hashedPassword);
+
+    res.redirect("/");
   },
 };
 
