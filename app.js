0ffcc7ac5c4a2922e4a8d46e89ca50e0b7acfc9a
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-16 14:38:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { router } from "./routes/routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use("/", router);
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Server running on port ${PORT}`);
+});
