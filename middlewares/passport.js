b249afc4445e87486e13cc8f743bb2fdd1d329d5
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-17 15:11:23 +0100

    Created a passport login strategy

---
+++ middlewares/passport.js
@@ -0,0 +1,39 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import db from "../db/queries.js";
+import bcrypt from "bcryptjs";
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUser(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserId(id);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
