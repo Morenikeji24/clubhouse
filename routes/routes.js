aef11f81f9c82afc8ca40c6a6a54852e5844c4a7
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-17 15:12:01 +0100

    Added routes for login

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import controller from "../controllers/controller.js";
 import { signUpValidateForm } from "../middlewares/signUpFormValidator.js";
+import { loginFormValidator } from "../middlewares/loginFormValidator.js";
 
 export const router = Router();
 
 router.get("/", controller.getHomePage);
 router.get("/sign-up", controller.getSignUp);
 router.post("/sign-up", signUpValidateForm, controller.postSignUp);
+router.get("/login", controller.getLogin);
+router.post("/login", loginFormValidator, controller.postLogin);
