eb7a1f01b116a880717ca5199c8ddaeea653ca23
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-20 14:53:20 +0100

    Added routes for getting the message form and posting the message form

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import controller from "../controllers/controller.js";
 import { signUpValidateForm } from "../middlewares/signUpFormValidator.js";
 import { loginFormValidator } from "../middlewares/loginFormValidator.js";
+import { requireLogin } from "../middlewares/requireLogin.js";
 
 export const router = Router();
 
@@ -10,3 +11,5 @@ router.get("/sign-up", controller.getSignUp);
 router.post("/sign-up", signUpValidateForm, controller.postSignUp);
 router.get("/login", controller.getLogin);
 router.post("/login", loginFormValidator, controller.postLogin);
+router.get("/messages/new", requireLogin, controller.getMessageForm);
+router.post("/messages/new", requireLogin, controller.postMessageForm);
