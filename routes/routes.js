c66b227fd37799292c0580d8e67db6f6972e0f17
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-20 17:09:05 +0100

    Added routes to handle creating members, admins and deleting messages

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ import controller from "../controllers/controller.js";
 import { signUpValidateForm } from "../middlewares/signUpFormValidator.js";
 import { loginFormValidator } from "../middlewares/loginFormValidator.js";
 import { requireLogin } from "../middlewares/requireLogin.js";
+import { memberFormValidator } from "../middlewares/memberFormValidator.js";
 
 export const router = Router();
 
@@ -14,3 +15,19 @@ router.post("/login", loginFormValidator, controller.postLogin);
 router.get("/messages/new", requireLogin, controller.getMessageForm);
 router.post("/messages/new", requireLogin, controller.postMessageForm);
 router.get("/logout", requireLogin, controller.logout);
+router.get("/become-member", requireLogin, controller.getMemberForm);
+router.post(
+  "/become-member",
+  requireLogin,
+  memberFormValidator,
+  controller.postMemberForm,
+);
+router.get("/become-admin", requireLogin, controller.getAdminForm);
+router.post(
+  "/become-admin",
+  requireLogin,
+  memberFormValidator,
+  controller.postAdminForm,
+);
+
+router.post("/messages/:id/delete", requireLogin, controller.deleteMessage);
