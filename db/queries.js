362aae2b84dfafb5efb063215e531dfa3d792064
Morenikeji24 <snipingkoala447@gmail.com>
2026-09-20 14:50:35 +0100

    Added queries for creating a new message and getting all messages

--- db/queries.js
+++ db/queries.js
@@ -23,6 +23,29 @@ const db = {
     ]);
     return rows[0];
   },
+
+  async addMessage(title, text, userId) {
+    await pool.query(
+      `INSERT INTO messages (title, text, user_id)
+        VALUES ($1, $2, $3)`,
+      [title, text, userId],
+    );
+  },
+
+  async getAllMessages() {
+    const { rows } = await pool.query(`
+        SELECT 
+        messages.*,
+        users.first_name,
+        users.last_name
+        FROM messages
+        JOIN users
+        ON messages.user_id = users.id
+        ORDER BY messages.time_stamp DESC
+        `);
+
+    return rows;
+  },
 };
 
 export default db;
